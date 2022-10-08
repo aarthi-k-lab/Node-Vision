d2e4a7ba95699b50ebaaa5b42e6b7a29c6423c9d
Aarthi <aarthikannan2103@gmail.com>
2022-10-08 17:50:41 +0530

    Integrates the AWS Recognition api with uploaded image to detect labels

--- routes/vision.js
+++ routes/vision.js
@@ -1,17 +1,38 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const AWS =require("../config/aws/config")
+const client = new AWS.Rekognition();
 
-router.post('/classify', function(req, res, next) {
-  // DON'T return the hardcoded response after implementing the backend
-  let response = ["shoe", "red", "nike"];
+router.post("/classify", async function (req, res) {
+  const file = req.files.file;
+  const data = file.data;
+  const image = Buffer.from(data, "base64");
 
-  // Your code starts here //
-
-  // Your code ends here //
-
-  res.json({
-    "labels": response
-  });
+  try {
+    response = await detectLables(image, 10, 95);
+    const labels = response.Labels.map((label) => label.Name);
+    console.log(labels.length)
+    if (!labels.length)  throw {message:"Unable to find the labels for the given image", status: 204 }
+    return res.status(200).json({ labels: labels });
+  } catch (err) {
+    console.error("Error Log", err);
+    const statusCode=err.status || 500
+    return res.status(statusCode).json({ error: err.message || "Unable to process the request" });
+  }
 });
+
+const detectLables = (image, MaxLabels, MinConfidence=75) => {
+  const params = {
+    Image: { Bytes: image },
+    MaxLabels,
+    MinConfidence,
+  };
+  return new Promise((resolve,reject)=>{
+    client.detectLabels(params, function(err, response) {
+      if (err) return reject(err)
+      resolve(response)
+    })
+  })
+};
 
 module.exports = router;
